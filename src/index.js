c60c091b01afe32a418bd5b60b1f6c7cb528f455
Tinsaye Simeneh <tinsayesimeneh608@gmail.com>
2022-11-17 21:47:23 +0300

    bootstrap links added to index file

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import Home from './components/home';
+import 'bootstrap/dist/css/bootstrap.css';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
