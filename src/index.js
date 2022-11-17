a1ec227119f6f36724bbe70da53cf1292e903d82
Tinsaye Simeneh <tinsayesimeneh608@gmail.com>
2022-11-17 13:08:03 +0300

    Home page component file added to index.js

--- src/index.js
+++ src/index.js
@@ -1,8 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import Home from './components/home';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
- 
+ <Home/>
 );
 
