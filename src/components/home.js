7e377f77204b489b6f4df8ebe36fb7419953f96b
Tinsaye Simeneh <tinsayesimeneh608@gmail.com>
2022-11-17 21:48:22 +0300

    container bg color updated

--- src/components/home.js
+++ src/components/home.js
@@ -2,7 +2,7 @@ import React from 'react';
 
 const Home = () => {
     return (
-        <div className='container'>
+        <div className='container-fluid mt-5 bg-primary'>
         <div className='row'>
             <div className='col-md-6'>
                 <h1>Home</h1>
