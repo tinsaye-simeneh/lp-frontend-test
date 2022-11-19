9129bc892d1486f6a156936d242b43f4606fc74d
Tinsaye Simeneh <tinsayesimeneh608@gmail.com>
2022-11-19 21:46:19 +0300

    second paragraph to left column added

--- src/components/home.js
+++ src/components/home.js
@@ -7,6 +7,7 @@ const Home = () => {
             <div className='col-md-6 bg-warning pt-5'>
                 <h1 className='ps-5'>Welcome to #Brand Name</h1>
                 <p> A place where access your hidden skills </p>
+                <p> Register and get a free access to our Resources </p>
             </div>
         </div>
         </div>
