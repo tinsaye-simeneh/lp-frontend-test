20b470e86b63247c2d70bb0ab898a413e2a0a8b4
Tinsaye Simeneh <tinsayesimeneh608@gmail.com>
2022-11-19 22:14:00 +0300

    left column style fixed

--- src/components/home.js
+++ src/components/home.js
@@ -4,10 +4,10 @@ const Home = () => {
     return (
         <div className='container-fluid mt-5 bg-white'>
         <div className='row'>
-            <div className='col-md-6 bg-white pt-5 ps-5 mt-5'>
+            <div className='col-md-6 bg-white pt-5 ps-5 mt-5 border pb-5'>
                 <h1 className='ps-5 mt-5'>Welcome to #Brand Name</h1>
                 <p className='ps-5 mt-5'> A place where access your hidden skills </p>
-                <p className='ps-5 mt-3'> Register and get a free access to our Resources </p>
+                <p className='ps-5 mt-3 pb-5'> Register and get a free access to our Resources </p>
             </div>
         </div>
         </div>
