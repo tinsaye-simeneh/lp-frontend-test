bd962a336b175ec4eff72d4cb4f6af992ff2f335
Tinsaye Simeneh <tinsayesimeneh608@gmail.com>
2022-11-20 19:09:45 +0300

    card template created

--- src/components/courses.js
+++ src/components/courses.js
@@ -10,6 +10,19 @@ const CoursePage = () => {
           Popular courses{" "}
         </h1>
       </div>
+
+    <div className="row">
+        <div className="col-md-3 col-12 mt-5">
+            <div className="card">
+              <div className="card-body">
+                    <h5 className="card-title"> FRONTEND </h5>
+                    <p className="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                    <a href="#" className="btn btn-primary">Go somewhere</a>
+                </div>
+                </div>
+                </div>
+    </div>
+
     </div>
   );
 };
