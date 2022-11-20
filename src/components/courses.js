adb71fe17f234b373b2075673528de6aafcdaa21
Tinsaye Simeneh <tinsayesimeneh608@gmail.com>
2022-11-20 19:06:08 +0300

    some minor error fixed inside course.js file

--- src/components/courses.js
+++ src/components/courses.js
@@ -1,8 +1,12 @@
 import React from 'react'
 import '../styles/index.css'
 
-const CoursePage = () => (
-       
-)
+const CoursePage = () => {
+       return(
+        <div className="container mt-5 bg-white">
+
+        </div>
+       )
+    }
 
 export default CoursePage;
