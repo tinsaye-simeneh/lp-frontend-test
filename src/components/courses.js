d97d70283f146cc865549cab07b9ecac25016549
Tinsaye Simeneh <tinsayesimeneh608@gmail.com>
2022-11-20 19:08:18 +0300

    formatting fixed in courses file

--- src/components/courses.js
+++ src/components/courses.js
@@ -1,14 +1,17 @@
-import React from 'react'
-import '../styles/index.css'
+import React from "react";
+import "../styles/index.css";
 
 const CoursePage = () => {
-       return(
-        <div className="container mt-5 bg-white">
-            <div className="row">
-                <h1 className='text-center' style={{fontSize: '26px'}}> Popular courses </h1>
-                </div>
-        </div>
-       )
-    }
+  return (
+    <div className="container mt-5 bg-white">
+      <div className="row">
+        <h1 className="text-center" style={{ fontSize: "26px" }}>
+          {" "}
+          Popular courses{" "}
+        </h1>
+      </div>
+    </div>
+  );
+};
 
 export default CoursePage;
