90aae0046ec7947d4917d2c78bd6869ddc64196a
Tinsaye Simeneh <tinsayesimeneh608@gmail.com>
2022-11-20 19:22:36 +0300

    props created for card file

--- src/components/cards.js
+++ src/components/cards.js
@@ -1,5 +1,4 @@
 import React from "react";
-import ReactDOM from "react-dom/client";
 
 const Card = ({title, descriptionText, link, linkState}) => {
     return (
@@ -8,18 +7,18 @@ const Card = ({title, descriptionText, link, linkState}) => {
       <div className="col-md-3 col-12 mt-5 mx-auto">
         <div className="card text-center bg-secondary">
           <div className="card-body">
-            <h5 className="card-title text-white"> FRONTEND </h5>
+            <h5 className="card-title text-white"> {title} </h5>
             <p
               className="card-text mt-3 text-white"
               style={{ fontSize: "13px" }}
             >
-              Step by step guide to become a frontend developer in 2022
+              Step by step guide to become a {descriptionText} in 2022
             </p>
             <a
-              href="#frontend"
+              href={link}
               className="btn btn-white py-0 text-decoration-underline text-white"
             >
-              View
+              {linkState}
             </a>
           </div>
         </div>
