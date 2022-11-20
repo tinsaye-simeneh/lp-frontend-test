04e475286d7515e08d439b574ce18af81545e568
Tinsaye Simeneh <tinsayesimeneh608@gmail.com>
2022-11-20 19:07:31 +0300

    custom text-black class created

--- src/components/courses.js
+++ src/components/courses.js
@@ -4,7 +4,9 @@ import '../styles/index.css'
 const CoursePage = () => {
        return(
         <div className="container mt-5 bg-white">
-
+            <div className="row">
+                <h1 className='text-center'> Popular courses </h1>
+                </div>
         </div>
        )
     }
