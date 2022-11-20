6d9f9e8291a3e83bb911e187e44ae154415ca42d
Tinsaye Simeneh <tinsayesimeneh608@gmail.com>
2022-11-20 18:46:39 +0300

    external css file added

--- src/components/home.js
+++ src/components/home.js
@@ -1,11 +1,12 @@
 import React from "react";
 import homeIllust from "../assets/images/homeIllust.svg";
+import '../styles/index.css' 
 
 const Home = () => {
   return (
     <div className="container mt-5 bg-white">
       <div className="row">
-        <div className="col-md-6 bg-white pt-5 mt-5 pb-5">
+        <div className="col-md-6 col-12 text-center pt-5 mt-5 pb-5 bg-warning">
           <h1 className="mt-5">Welcome to #Brand Name</h1>
           <p className="mt-5"> A place where access your hidden skills </p>
           <p className="mt-3 pb-5">
@@ -13,19 +14,19 @@ const Home = () => {
             Register and get a free access to our Resources{" "}
           </p>
           <div className="row">
-            <div className="col-md-3">
-              <div className="btn btn-white border border-primary px-3" style={{fontWeight:'500'}}>
+            <div className="col-md-3 col-12 get-btn">
+              <div className="btn btn-white border border-primary px-4 py-2" style={{fontWeight:'500'}}>
                 Get Started
               </div>
             </div>
-            <div className="col-md-5">
+            <div className="col-md-5 col-12">
               <div className="btn btn-white" style={{fontWeight:'500'}}>
                 Found Out More
               </div>
             </div>
           </div>
         </div>
-        <div className="col-md-6 pt-3 bg-white mt-5 pt-5">
+        <div className="col-md-6 col-12 pt-3 bg-white mt-5 pt-5">
           <img src={homeIllust} alt="home" className="img-fluid" />
         </div>
       </div>
