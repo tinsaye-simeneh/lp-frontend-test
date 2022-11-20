cd3111b7e4c13db692ff9bb8770567badc94318c
Tinsaye Simeneh <tinsayesimeneh608@gmail.com>
2022-11-20 19:05:07 +0300

    minor formatting

--- src/index.js
+++ src/index.js
@@ -1,8 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import 'bootstrap/dist/css/bootstrap.css';
+
 import Home from './components/home';
 import CoursePage from './components/courses';
-import 'bootstrap/dist/css/bootstrap.css';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
