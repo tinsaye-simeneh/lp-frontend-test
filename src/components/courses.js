1c8179fc7e4f70c4603b9ca79a0f48ad36019f97
Tinsaye Simeneh <tinsayesimeneh608@gmail.com>
2022-11-20 19:03:59 +0300

    course page created

---
+++ src/components/courses.js
@@ -0,0 +1,8 @@
+import React from 'react'
+import '../styles/index.css'
+
+const CoursePage = () => (
+       
+)
+
+export default CoursePage;
--- src/index.js
+++ src/index.js
@@ -1,10 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import Home from './components/home';
+import CoursePage from './components/courses';
 import 'bootstrap/dist/css/bootstrap.css';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
- <Home/>
+ <CoursePage/>
 );
 
