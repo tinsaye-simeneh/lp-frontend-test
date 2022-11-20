119bb064184753fbf7105bbaa81cbabb4768b2b5
Tinsaye Simeneh <tinsayesimeneh608@gmail.com>
2022-11-20 18:58:37 +0300

    home page responsiveness final commit

--- src/components/home.js
+++ src/components/home.js
@@ -1,6 +1,7 @@
 import React from "react";
-import homeIllust from "../assets/images/homeIllust.svg";
+
 import '../styles/index.css' 
+import homeIllust from "../assets/images/homeIllust.svg";
 
 const Home = () => {
   return (
