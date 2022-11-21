d10e45a678d924d91b3915e2138631e82ffdbff9
Tinsaye Simeneh <tinsayesimeneh608@gmail.com>
2022-11-21 09:03:47 +0300

    second navbar added

--- src/components/navbar/navbar.js
+++ src/components/navbar/navbar.js
@@ -2,14 +2,30 @@ import React from "react";
 
 const Navbar = () => {
     return(
-        <div className="container-fluid">
+        <div className="container">
             <div className="row">
                 <div className="col-12">
-                    <nav className="navbar navbar-expand-lg navbar-light bg-white">
-                        <a className="navbar-brand" href="#">#Brand Name</a>
+                    <nav className="navbar navbar-expand-lg navbar-light bg-warning">
+                        <a className="navbar-brand ps-3" href="#logo">Logo</a>
                         <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
                             <span className="navbar-toggler-icon"></span>
                         </button>
+                        <div className="collapse navbar-collapse" id="navbarSupportedContent">
+                            <ul className="d-flex navbar-nav ms-auto mb-2 mb-lg-0 me-3">
+                                <li className="nav-item">
+                                    <a className="nav-link active" aria-current="page" href="#home">Home</a>
+                                </li>
+                                <li className="nav-item">
+                                    <a className="nav-link" href="#courses">Courses</a>
+                                </li>
+                                <li className="nav-item">
+                                    <a className="nav-link" href="#about">About</a>
+                                </li>
+                                <li className="nav-item">
+                                    <a className="nav-link" href="#contact">Contact</a>
+                                </li>
+                            </ul>
+                        </div>
                         </nav>
                 </div>
             </div>
