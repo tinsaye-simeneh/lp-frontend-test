aeb16a24313491ca865143cfd1456cf0291c8938
Tinsaye Simeneh <tinsayesimeneh608@gmail.com>
2022-11-21 09:33:11 +0300

    top nav file created

---
+++ src/components/navbar/top-nav.js
@@ -0,0 +1,34 @@
+import React from "react";
+
+const TopNav = () => {
+    return(
+        <div className="container">
+        <div className="row">
+            <div className="col-12">
+                <nav className="navbar navbar-expand-lg navbar-white bg-white">
+                    <a className="navbar-brand ps-5" href="#logo">Logo</a>
+                    <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+                        <span className="navbar-toggler-icon"></span>
+                    </button>
+                    <div className="collapse navbar-collapse" id="navbarSupportedContent">
+                        <ul className="d-flex navbar-nav ms-auto mb-2 mb-lg-0 me-3">
+                            <li className="nav-item">
+                                <a className="nav-link active" aria-current="page" href="#courses">Courses</a>
+                            </li>
+                            <li className="nav-item">
+                                <a className="nav-link" href="#resources">Resources</a>
+                            </li>
+                            <li className="nav-item">
+                                <a className="nav-link" href="#rules">Rules</a>
+                            </li>
+                            <li className="nav-item">
+                                <a className="nav-link" href="#usefulLinks">Useful links</a>
+                            </li>
+                        </ul>
+                    </div>
+                    </nav>
+            </div>
+        </div>
+    </div>            
+    )
+}
