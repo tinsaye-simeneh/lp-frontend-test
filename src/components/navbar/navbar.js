96a8a6b0e1ef25dc770dce3955cace6747fcd712
Tinsaye Simeneh <tinsayesimeneh608@gmail.com>
2022-11-21 09:07:18 +0300

    second navbar bg chnaged to white

--- src/components/navbar/navbar.js
+++ src/components/navbar/navbar.js
@@ -6,7 +6,7 @@ const Navbar = () => {
         <div className="container">
             <div className="row">
                 <div className="col-12">
-                    <nav className="navbar navbar-expand-lg navbar-white bg-warning">
+                    <nav className="navbar navbar-expand-lg navbar-white bg-white">
                         <a className="navbar-brand ps-5" href="#logo">Logo</a>
                         <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
                             <span className="navbar-toggler-icon"></span>
@@ -14,7 +14,7 @@ const Navbar = () => {
                         <div className="collapse navbar-collapse" id="navbarSupportedContent">
                             <ul className="d-flex navbar-nav ms-auto mb-2 mb-lg-0 me-3">
                                 <li className="nav-item">
-                                    <a className="nav-link" href="#courses">Courses</a>
+                                    <a className="nav-link active" href="#courses">Courses</a>
                                 </li>
                                 <li className="nav-item">
                                     <a className="nav-link" href="#about">Resources</a>
@@ -23,7 +23,7 @@ const Navbar = () => {
                                     <a className="nav-link" href="#contact">Rules</a>
                                 </li>
                                 <li className="nav-item">
-                                    <a className="nav-link active" aria-current="page" href="#home">Useful links</a>
+                                    <a className="nav-link" aria-current="page" href="#home">Useful links</a>
                                 </li>
                             </ul>
                         </div>
