592c0ba111194013189819716e459942c3c58624
Tinsaye Simeneh <tinsayesimeneh608@gmail.com>
2022-11-21 09:06:38 +0300

    navbar style file added

--- src/components/navbar/navbar.js
+++ src/components/navbar/navbar.js
@@ -1,28 +1,29 @@
 import React from "react";
+import '../../styles/navbar.css';
 
 const Navbar = () => {
     return(
         <div className="container">
             <div className="row">
                 <div className="col-12">
-                    <nav className="navbar navbar-expand-lg navbar-light bg-warning">
-                        <a className="navbar-brand ps-3" href="#logo">Logo</a>
+                    <nav className="navbar navbar-expand-lg navbar-white bg-warning">
+                        <a className="navbar-brand ps-5" href="#logo">Logo</a>
                         <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
                             <span className="navbar-toggler-icon"></span>
                         </button>
                         <div className="collapse navbar-collapse" id="navbarSupportedContent">
                             <ul className="d-flex navbar-nav ms-auto mb-2 mb-lg-0 me-3">
                                 <li className="nav-item">
-                                    <a className="nav-link active" aria-current="page" href="#home">Home</a>
-                                </li>
-                                <li className="nav-item">
                                     <a className="nav-link" href="#courses">Courses</a>
                                 </li>
                                 <li className="nav-item">
-                                    <a className="nav-link" href="#about">About</a>
+                                    <a className="nav-link" href="#about">Resources</a>
                                 </li>
                                 <li className="nav-item">
-                                    <a className="nav-link" href="#contact">Contact</a>
+                                    <a className="nav-link" href="#contact">Rules</a>
+                                </li>
+                                <li className="nav-item">
+                                    <a className="nav-link active" aria-current="page" href="#home">Useful links</a>
                                 </li>
                             </ul>
                         </div>
