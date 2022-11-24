fee395501136eb277f309c4ae339ebe938cabcd9
Tinsaye Simeneh <tinsayesimeneh608@gmail.com>
2022-11-24 19:45:33 +0300

    nav tag fixed for top nav

--- src/components/navbar/top-nav.js
+++ src/components/navbar/top-nav.js
@@ -5,8 +5,8 @@ const TopNav = () => {
     return(
         <div className="container">
         <div className="row ">
-            <div className="col-12">
-                <nav className="navbar navbar-expand-lg navbar-white bg-black">
+        <nav className="navbar navbar-expand-lg navbar-white bg-black">
+            <div className="col-9">
                     <div className="collapse navbar-collapse" id="navbarSupportedContent">
                         <ul className="d-flex navbar-nav ml-auto mb-2 mb-lg-0 ms-5">
                             <li className="nav-item">
@@ -20,8 +20,12 @@ const TopNav = () => {
                             </li>
                         </ul>
                     </div>
-                    </nav>
             </div>
+            <div className="col-3 bg-black ms-auto">
+                <img className="img-fluid" src="https://www.theschooloflife.com/wp-content/uploads/2019/11/Logo-1.png" alt="logo"/>
+            </div>
+            
+            </nav>
         </div>
     </div>            
     )
